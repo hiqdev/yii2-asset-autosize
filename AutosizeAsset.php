5084ae349899f59e7f9f6f1ca7313efacc27fad8
Andrii Vasyliev <sol@hiqdev.com>
2015-05-22 20:55:17 +0000

    + actual asset file

---
+++ AutosizeAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link      http://hiqdev.com/yii2-asset-autosize
+ * @license   BSD 3-clause
+ * @copyright Copyright (c) 2015 HiQDev
+ */
+
+namespace hiqdev\assets\autosize;
+
+class AutosizeAsset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@bower/autosize';
+
+    public $js = [
+        'dest/autosize.min.js',
+    ];
+
+}
