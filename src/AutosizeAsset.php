2812ae1614dfc18c5a39a3b0e430d6c54c65fa60
Andrii Vasyliev <sol@hiqdev.com>
2016-04-08 07:51:57 +0000

    phpcsfixed

--- src/AutosizeAsset.php
+++ src/AutosizeAsset.php
@@ -1,12 +1,12 @@
 <?php
 
 /*
- * Yii 2 asset for bower autosize
+ * Yii2 asset for bower autosize
  *
  * @link      https://github.com/hiqdev/yii2-asset-autosize
  * @package   yii2-asset-autosize
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015, HiQDev (https://hiqdev.com/)
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
  */
 
 namespace hiqdev\assets\autosize;
--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * Yii2 asset for bower autosize
+ *
+ * @link      https://github.com/hiqdev/yii2-asset-autosize
+ * @package   yii2-asset-autosize
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
 error_reporting(E_ALL & ~E_NOTICE);
 
 require_once __DIR__ . '/../vendor/autoload.php';
